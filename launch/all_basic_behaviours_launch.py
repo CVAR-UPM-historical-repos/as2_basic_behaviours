de45cdf6f133bd0238254074806e4f24ed6926ca
Pedro Arias <pedroariasperez96@gmail.com>
2022-04-25 18:17:31 +0200

    Updating launch with new takeoff and land

--- launch/all_basic_behaviours_launch.py
+++ launch/all_basic_behaviours_launch.py
@@ -1,25 +1,40 @@
+from os.path import join
+
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
+from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
+    config_takeoff = join(
+        get_package_share_directory('takeoff_behaviour'),
+        'config',
+        'takeoff_behaviour.yaml'
+    )
+    config_land = join(
+        get_package_share_directory('land_behaviour'),
+        'config',
+        'land_behaviour.yaml'
+    )
     return LaunchDescription([
         DeclareLaunchArgument('drone_id', default_value='drone0'),
         Node(
-            package='as2_basic_behaviours',
+            package='takeoff_behaviour',
             executable='takeoff_behaviour_node',
             name='takeoff_behaviour_node',
             namespace=LaunchConfiguration('drone_id'),
+            parameters=[config_takeoff],
             output='screen',
             emulate_tty=True
         ),
         Node(
-            package='as2_basic_behaviours',
+            package='land_behaviour',
             executable='land_behaviour_node',
             name='land_behaviour_node',
             namespace=LaunchConfiguration('drone_id'),
+            parameters=[config_land],
             output='screen',
             emulate_tty=True
         ),
